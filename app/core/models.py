58e76066f07c4724c4953576a16d3996302ce8e3
ShivamSingh226 <shivamsingh226@gmail.com>
2025-01-30 15:51:23 +0530

    Corrected linting for the code

--- app/core/models.py
+++ app/core/models.py
@@ -9,23 +9,22 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for the users"""
 
-    def create_user(self,email,password=None, **extra_field):
+    def create_user(self, email, password=None, **extra_field):
         """Create a user"""
-        user=self.model(email=self.normalize_email(email),**extra_field)
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
 
-
-class User(AbstractBaseUser,PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
-    email=models.EmailField(max_length=255,unique=True)
-    name=models.CharField(max_length=255)
-    is_active=models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=False)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-    objects=UserManager()
+    objects = UserManager()
 
-    USERNAME_FIELD='email'
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,13 +4,14 @@ Tests for User Models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test Models"""
 
     def test_create_user_with_email_successful(self):
-        email='test@example.com'
-        password='password123'
-        user=get_user_model().objects.create_user(
+        email = 'test@example.com'
+        password = 'password123'
+        user = get_user_model().objects.create_user(
             email=email,
             password=password,
         )
@@ -20,13 +21,13 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Create and validate emails"""
-        sample_Emails=[
-            ['test1@EXAMPLE.com','test1@example.com'],
-            ['TEST2@EXAMPLE.com','TEST2@example.com'],
-            ['teSt3@Example.com','teSt3@example.com'],
-            ['Test4@example.com','Test4@example.com']
+        sample_Emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['TEST2@EXAMPLE.com', 'TEST2@example.com'],
+            ['teSt3@Example.com', 'teSt3@example.com'],
+            ['Test4@example.com', 'Test4@example.com']
         ]
 
         for email, expected in sample_Emails:
-            user = get_user_model().objects.create_user(email,'sample123')
-            self.assertEqual(user.email,expected)
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
