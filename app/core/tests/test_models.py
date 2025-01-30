c0258e8698d60f2c4e93b823f41d721c55a40d47
ShivamSingh226 <shivamsingh226@gmail.com>
2025-01-30 15:42:27 +0530

    Corrected code with UserModel object is not callable and inheriting test Case instead of SimpleTestCase

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,10 +1,10 @@
 """
 Tests for User Models
 """
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTests(SimpleTestCase):
+class ModelTests(TestCase):
     """Test Models"""
 
     def test_create_user_with_email_successful(self):
@@ -28,5 +28,5 @@ class ModelTests(SimpleTestCase):
         ]
 
         for email, expected in sample_Emails:
-            user = get_user_model().objects().create_user(email,'sample123')
+            user = get_user_model().objects.create_user(email,'sample123')
             self.assertEqual(user.email,expected)
