3c8b287f673b489f56c387e335964879cbc162d7
ShivamSingh226 <shivamsingh226@gmail.com>
2025-02-06 00:45:37 +0530

    Viewsets and serializers updated for ingredients, update and delete api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -51,7 +51,10 @@ class TagViewSet(mixins.DestroyModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class IngredientViewSet(mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
 
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
